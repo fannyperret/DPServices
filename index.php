41075e38a1d30c512fd738a05c9755ac80c12ee0
Funny Rose <fannyperretlombard@gmail.com>
2018-01-02 23:19:44 +0100

    :snowman:

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fannyperret
+ * Date: 02/10/2017
+ * Time: 17:37
+ */ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dp Services Plombier-Dépannage</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="CSS/dpservices.css" type="text/css" media="screen"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!------A VOIR SI FONCTIONNE AVEC OU SANS EUX ----------------
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    ----------->
+
+</head>
+<body>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>DP SERVICES : PLOMBERIE & DEPANNAGE</h1>
+        <p>Plombier dans la Drôme (26)</p>
+    </div>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Dp Services</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Accueil</a></li>
+                <li><a href="#">Salles de bain</a></li>
+                <li><a href="#">Electricité</a></li>
+                <li><a href="#">Menuiserie</a></li>
+                <li><a href="#">Contactez-moi !</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="image_robinet">
+    <div class="container" style="margin-top:50px">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                <h2>ACTIVITES : VENTE / INSTALLATION / DEPANNAGE</h2>
+                <p>Plombier professionnel je suis à votre écoute pour refaire votre salle de bain ou pour un dépannage en toute tranquillité !</p>
+                <h3>Pour prendre rendez-vous :</h3>
+                <p>Fixe : 04 75 59 57 67<br/>
+                    Portable : 06 80 41 58 25<br/>
+                    Email : contact@dpservices26.fr</p>
+                <h3>Mon adresse :</h3>
+                <p>17 rue des Constellations<br/>
+                    26 760<br/>
+                    BEAUMONT-LES-VALENCE</p>
+            </div>
+        </div>
+    </div>
+    </section>
+
+    <section class="image_robinet">
+    <footer class="container-fluid">
+        <div class="copyrights">
+            <p>Dp Services © 2017, Tous Droits Réservés
+                <br>
+                <span>Site créé par : Fanny Perret</span></p>
+            <p><a href="https://www.linkedin.com/in/fanny-perret-678469104/" target="Linkedin">Web Developer<i class="fa fa-linkedin-square" aria-hidden="true"></i> </a></p>
+        </div>
+    </footer>
+    </section>
+
+
+</div>
+</body>
+
